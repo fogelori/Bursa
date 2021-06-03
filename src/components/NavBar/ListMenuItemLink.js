d7c6abd167b2d2f58eb7dabd171c6dacb8ae20de
Ori <fogelori@gmail.com>
2021-06-03 23:02:04 +0300

    revert

--- src/components/NavBar/ListMenuItemLink.js
+++ src/components/NavBar/ListMenuItemLink.js
@@ -2,18 +2,16 @@ import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import { Link as RouterLink, useRouteMatch } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
 function ListMenuItemLink(props) {
   //took from https://material-ui.com/guides/composition/
   const { icon, primary, to } = props;
-  let { path } = useRouteMatch();
-  path = path === "/" ? "" : path + "/";
 
   const renderLink = React.useMemo(
     () =>
       React.forwardRef((itemProps, ref) => (
-        <RouterLink to={`${path}${to}`} ref={ref} {...itemProps} />
+        <RouterLink to={to} ref={ref} {...itemProps} />
       )),
     [to]
   );
--- src/components/Router/RenderRoutes.js
+++ src/components/Router/RenderRoutes.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Routes from "./Routes";
 
 function RenderRoutes() {
-  let { path } = useRouteMatch();
-  path = path === "/" ? "" : path + "/";
   return (
     <Switch>
       {Routes.map((route, index) => {
         const TagName = route.component;
         return (
-          <Route exact={route.exact} path={`${path}${route.path}`} key={index}>
+          <Route exact={route.exact} path={route.path} key={index}>
             <TagName />
           </Route>
         );
